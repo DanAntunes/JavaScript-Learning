39f6683013ca5ef3636e6c892654e18a86be63bc
Soren <danilo-258@hotmail.com>
2024-11-09 14:52:34 -0300

    feat(js): desafio 2 de laço de repetição numeros pares

---
+++ src/scripts/for/desafio2.js
@@ -0,0 +1,9 @@
+//impressão de numeros pares
+const numeros = [];
+
+for (let i = 0; i < numeros.length; i++) {
+  const numero = numeros[i];
+  if (numero % 2 === 0) {
+    console.log(numero);
+  }
+};
